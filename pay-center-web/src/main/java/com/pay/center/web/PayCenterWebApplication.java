ac0a93555f35e4ee221a8739970f791d891cbb0d
chenwei <cw961590280@hotmail.com>
2018-12-05 14:59:50 +0800

    x

--- pay-center-web/src/main/java/com/pay/center/web/PayCenterWebApplication.java
+++ pay-center-web/src/main/java/com/pay/center/web/PayCenterWebApplication.java
@@ -1,9 +1,11 @@
 package com.pay.center.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages={"com.pay.*"})
+@MapperScan("com.pay.center.core.dao")
 public class PayCenterWebApplication {
 
     public static void main(String[] args) {
