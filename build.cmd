ad49cf5b1783eee6648bc1654920e7b3a8dde726
chenwei <cw961590280@hotmail.com>
2018-12-24 16:33:08 +0800

    x

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 
-mvn clean package -Dmaven.skip.test=true
+call mvn clean package -Dmaven.skip.test=true
 
 docker build -t=registry.cn-hangzhou.aliyuncs.com/cvi/pay-center .
 docker push registry.cn-hangzhou.aliyuncs.com/cvi/pay-center
