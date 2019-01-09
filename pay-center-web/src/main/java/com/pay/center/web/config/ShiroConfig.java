f33655a59d46130d8a8883789f66fd0fde313193
chenwei <cw961590280@hotmail.com>
2019-01-09 18:12:45 +0800

    x

--- pay-center-web/src/main/java/com/pay/center/web/config/ShiroConfig.java
+++ pay-center-web/src/main/java/com/pay/center/web/config/ShiroConfig.java
@@ -106,6 +106,8 @@ public class ShiroConfig {
         // 配置不会被拦截的链接 顺序判断
         filterChainDefinitionMap.put("/static/**", "anon");
         filterChainDefinitionMap.put("/login", "anon");
+
+
         filterChainDefinitionMap.put("/**", "authc");
 
         //未登陆跳转地址
