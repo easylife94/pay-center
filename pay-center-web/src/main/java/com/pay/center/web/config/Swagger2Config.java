fc21109e0ed254db43964f04be52c2f49d1c1ce6
chenwei <chenwei@qq.com>
2019-01-12 22:41:09 +0800

    x

---
+++ pay-center-web/src/main/java/com/pay/center/web/config/Swagger2Config.java
@@ -0,0 +1,28 @@
+package com.pay.center.web.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * swagger2配置
+ *
+ * @author chenwei
+ * @date 2019-01-12
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket swaggerSpringMvcPlugin() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .build();
+    }
+}
